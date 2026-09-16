477e4d5149d9a16ebd08cc0865e33bcf0d2c08eb
julesaltaras <jules.altaras9@etu.univ-lorraine.fr>
2026-09-16 17:30:48 +0200

    classe identité complète

--- Formation/src/Identite.java
+++ Formation/src/Identite.java
@@ -1,2 +1,24 @@
 public class Identite {
+    private String nom;
+    private String prenom;
+    private String nip;
+
+    public Identite(String nom,String prenom,String nip)
+    {
+        this.nom=nom;
+        this.prenom=prenom;
+        this.nip=nip;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getNip(){
+        return nip;
+    }
 }
