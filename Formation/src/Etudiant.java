9218cd61c7339bd7242848b9def0e3292d3126fb
julesaltaras <jules.altaras9@etu.univ-lorraine.fr>
2026-09-22 15:59:31 +0200

    fin classe étudiant

--- Formation/src/Etudiant.java
+++ Formation/src/Etudiant.java
@@ -1,44 +1,70 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Etudiant {
-    private  Identite id;
+
+    private Identite id;
+
     private Formation form;
-    private Map<Matiere, List<Double> resultats>;
+
+    private Map<Matiere, List<Double>> resultats;
+
     public Etudiant(Identite identite, Formation formation) {
-        this.id=identite;
-        this.form=formation;
-        this.resultats=new HashMap<>();
+        this.id = identite;
+        this.form = formation;
+        this.resultats = new HashMap<>();
     }
 
     public Identite getId() {
         return id;
     }
 
-    public void ajouterNote(Matiere m,double note){
-        if (note>20){
-            note=20;
+    public void ajouterNote(Matiere m, double note){
+        if (note > 20){
+            note = 20;
         }
-        if (note<0){
-            note=0;
+        if (note < 0){
+            note = 0;
         }
-        if (!resultats.containKey(m)){
-            resultats.put(m,new ArrayList<>());
+        if (!resultats.containsKey(m)) {
+            resultats.put(m, new ArrayList<>()); // initialise une liste vide si absente
         }
         resultats.get(m).add(note);
     }
-    
-    public calculerMoyenneMatiere(Matiere matiere){
+
+    public double calculerMoyenneMatiere(Matiere matiere){
+        int taille = this.resultats.get(matiere).size();
+        double  moyenneMatiere = 0;
+        for (int i = 0; i < taille; i++){
+            moyenneMatiere += this.resultats.get(matiere).get(i);
+        }
+        return moyenneMatiere/taille;
     }
 
-    public calculerMoyenneGenerale(){
+    public double calculerMoyenneGenerale(){
+        double somme = 0;
+        double totalCoeff = 0;
+        for (Matiere m : this.form.getMatieres().keySet()){
+            List<Double> notes = this.resultats.get(m);
+            double sommeNotes = 0;
+            for (Double note : notes){
+                sommeNotes += note;
+            }
+            double moyenne =  sommeNotes/notes.size();
+            int coeff = form.getCoefficient(m);
+            somme += moyenne * coeff;
+            totalCoeff += coeff;
+        }
+        if (totalCoeff == 0){
+            return 0;
+        }
+        else return somme/totalCoeff;
     }
 
     public Formation getFormation() {
         return form;
     }
 
-    public Map<Matier,List<Double>> getResultats(){
-        return resultats;
+    public Map<Matiere, List<Double>>  getResultats() {
+        return this.resultats;
     }
 }
